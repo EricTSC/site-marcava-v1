b335a2db634b209c41ad87666448091775c45b03
Eric Campos <114074487+EricTSC@users.noreply.github.com>
2023-03-21 16:08:49 -0300

    Initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Home(){
+    return (
+    
+    <h1>O que eu quiser</h1>
+    
+    );
+};
+
+
+
+export default Home;
