0dc0f6f8f0eae778aa78ea283a71d59e0c938f8f
Eric Campos <114074487+EricTSC@users.noreply.github.com>
2023-03-21 15:25:23 -0300

    iniciate

---
+++ src/pages/index.ts
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function home() {
+    let texto = "Qualquer texto que eu queira aqui"
+    return texto
+};
+
+export default home()
